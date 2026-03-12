2fed8441a4874efbc3be58b8898cac86fd52932b
it407 <it@zofffoods.com>
2026-03-12 14:41:46 +0530

    Update Google Sheets URL for CSV export

--- PPE.py
+++ PPE.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="AI CCTV Monitoring", layout="wide")
 
 st.title("AI CCTV Monitoring Dashboard")
 
-GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/18cfgyHMGpoNGLzqGalg7DIu0VFKtIihUh-Nzn2HQDKQ/edit?usp=sharing"
+GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/18cfgyHMGpoNGLzqGalg7DIu0VFKtIihUh-Nzn2HQDKQ/gviz/tq?tqx=out:csv&sheet=odata"
 
 
 @st.cache_data(ttl=60)
